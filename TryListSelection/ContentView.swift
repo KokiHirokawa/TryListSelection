fafb6137a69fce060c4b341b66b87e4a78a9fca0
KokiHirokawa <kitakitsune1110@gmail.com>
2023-02-28 17:37:37 +0900

    Initial Commit

---
+++ TryListSelection/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TryListSelection
+//
+//  Created by 廣川 昂紀 on 2023/02/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TryListSelection/TryListSelectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TryListSelectionApp.swift
+//  TryListSelection
+//
+//  Created by 廣川 昂紀 on 2023/02/28.
+//
+
+import SwiftUI
+
+@main
+struct TryListSelectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
